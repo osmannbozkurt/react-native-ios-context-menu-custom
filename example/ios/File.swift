8c0f09298551ae4de14a87e0344ae33181516d73
Osman Bozkurt <osmanbzkrt023@gmail.com>
2022-11-10 21:11:36 +0300

    Initial Commit

---
+++ example/ios/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  IosContextMenuExample
+//
+//  Created by Dominic Go on 11/12/21.
+//
+
+import Foundation
---
+++ react-native.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  project: {
+    android: null,
+    ios: {
+      // eslint-disable-next-line prettier/prettier
+      project: './ios/IosContextMenu.xcodeproj',
+    },
+  },
+};
